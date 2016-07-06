8c3adba2aa55830fd03f1caf9d5b23ce2172cb0b
Gisli Svavarsson <Gisli Svavarsson>
2016-07-06 17:15:03 +0000

    numbers

--- Umbraco.Site/scripts/src/numbers.js
+++ Umbraco.Site/scripts/src/numbers.js
@@ -1,17 +1,45 @@
 app.numbersgame = {
 	init: function() {
 		var me = this;
-		$('body').on('click', function() {
-			console.log('fireworks');
-			$('.numbers-games .numberr').each(function(indx, item) {
-				var count = $(this).data('number'),
-					numAnim = new CountUp(this, 0, count);
+		var hasCountend = false;
+		// init controller
 
-				numAnim.start();
-				//me.countup($(item).find('#number-input').val(), $(item).find('#number-unit').val());
-			});
-		});
-	}, 
+
+			var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 100}});
+			
+			// build scenes
+			new ScrollMagic.Scene({triggerElement: ".FP-numberss"})
+				.addTo(controller)
+				.on("end", function (e) {
+					if (!hasCountend) {
+						$('.numbers-games .numberr').each(function(indx, item) {
+							var unit = '';
+							var separator = '.';
+							if ($(item).data('isyear') == "isYear") {
+								separator = '';
+							}
+							if ($(item).data('unit') != '') {
+								unit = $(item).data('unit');
+							}
+							var options = {
+								useEasing : true, 
+								useGrouping : true, 
+								separator : separator, 
+								decimal : ',', 
+								prefix : '', 
+								suffix : ' ' + unit 
+							}
+							var count = $(this).data('number'),
+								numAnim = new CountUp(this, 0, count, 0, 2.5, options);
+
+							numAnim.start();
+							//me.countup($(item).find('#number-input').val(), $(item).find('#number-unit').val());
+						});
+						hasCountend = true;
+					}
+				});
+	},
+
 	countup: function(numm, unit) {
 		var options = {
 			useEasing : true, 
